1d87ae28f8bb5d24ad479ea61b6a8cc2ee30cc4d
DanielCasaresIglesias <dan.casares@gmail.com>
2025-08-19 14:27:41 -0700

    Removed unused import

--- frontend/src/pages/searchPage/components/ResultsColumn.tsx
+++ frontend/src/pages/searchPage/components/ResultsColumn.tsx
@@ -1,5 +1,5 @@
 // frontend/src/components/ResultsColumn.tsx
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import ExpandableSearch from './ExpandableSearch';
 import '../styles/resultsColumn.css';
 import { type Park } from '../../../types/park';
